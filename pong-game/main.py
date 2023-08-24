17496ebdc89a404ba3ede7d2dc7657ac99ca8f0b
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-08-24 17:52:00 +0330

    Create paddle class to make a second paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,10 +1,23 @@
-from turtle import Screen
-
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PONG")
+screen.tracer(0) #Turn off animation
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update() #Because we used tracer
 
 screen.exitonclick()
