f3ab9f7e1476bcf3484497853acc674752991de3
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-08-24 18:50:09 +0330

    How to Detect Collisions with the Paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -22,7 +22,6 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 
-
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
@@ -31,6 +30,11 @@ while game_is_on:
 
     #Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    #Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
 
 screen.exitonclick()
