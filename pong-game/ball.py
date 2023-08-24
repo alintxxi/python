aa33dd9895521e71221b5fc0683d9ab3fc7d1e29
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-08-25 01:19:24 +0330

    final project

--- pong-game/ball.py
+++ pong-game/ball.py
@@ -5,11 +5,12 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("circle")
         self.color("white")
+        self.shape("circle")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -21,7 +22,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
