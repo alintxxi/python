909c880c678c90f01a4c10d33a38951ff54274e5
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-08-24 18:13:35 +0330

    Write the Ball Class and Make the Ball Move

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
+from ball import Ball
 from paddle import Paddle
+import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +12,8 @@ screen.tracer(0) #Turn off animation
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -16,8 +21,12 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+
+
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update() #Because we used tracer
+    ball.move()
 
 screen.exitonclick()
