b68c4f7506b40514793cb3dc2074e2911f70dd1c
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-08-25 23:29:44 +0330

    Detect when the Turtle collides with a Car squish

--- turtle-crossing-game/main.py
+++ turtle-crossing-game/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -21,3 +22,13 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+    scoreboard.update_scoreboard()
+
+    # Detect collision with car.
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
--- turtle-crossing-game/scoreboard.py
+++ turtle-crossing-game/scoreboard.py
@@ -1,5 +1,24 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.color("black")
+        self.penup()
+        self.goto(-280, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER")
+
+
