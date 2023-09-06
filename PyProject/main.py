81b0b8407be0f79bab24acb61e1f36cc54951094
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-06 14:39:56 +0330

    Up-Down-Right-Left

--- PyProject/main.py
+++ PyProject/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
 
 screen = Screen()
@@ -10,15 +10,18 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
     snake.move()
-
-
-
 
 
 screen.exitonclick()
