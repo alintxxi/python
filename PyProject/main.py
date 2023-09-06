770629caae789979b3058bfa4e2d95bf71a592ca
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-06 19:53:29 +0330

    practice

--- PyProject/main.py
+++ PyProject/main.py
@@ -1,6 +1,7 @@
-import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +22,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
