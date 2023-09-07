813c791a5eb6a072b1ab8ab885755fcdc888d380
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-07 18:06:55 +0330

    practice

--- PyProject/main.py
+++ PyProject/main.py
@@ -28,6 +28,19 @@ while game_is_on:
 
     # Detect collision with wall.
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddles.
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when r_paddle miss.
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when l_paddle miss.
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 screen.exitonclick()
