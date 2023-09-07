55778387e14819e21a26c2c3c69ce3864334a21b
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-07 13:11:09 +0330

    practice

--- PyProject/main.py
+++ PyProject/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
 screen.onkey(r_paddle.down, "Down")
@@ -18,6 +22,8 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
