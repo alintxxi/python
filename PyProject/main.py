30d37a7ae4d725fcc187af158190f0abc79b85eb
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-07 18:39:18 +0330

    practice

--- PyProject/main.py
+++ PyProject/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,10 +10,13 @@ screen.bgcolor("black")
 screen.title("P⚪NG")
 screen.tracer(0)
 
+SPEED = 0.1
+
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -22,7 +26,7 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -37,10 +41,12 @@ while game_is_on:
     # Detect when r_paddle miss.
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect when l_paddle miss.
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
