40785b457b042ce9f6d9f1d8b475c8b6fd1169dd
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-08 22:55:08 +0330

    Add a High Score to the Snake Game

--- snake-game/main.py
+++ snake-game/main.py
@@ -26,26 +26,25 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        # scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    #Detect collision with tail with slice method.
+    # Detect collision with tail with slice method.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
-
-
-
-
+            # game_is_on = False
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
