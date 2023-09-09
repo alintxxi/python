d861c82dbfe13001a7bcf5dd657a541748630f49
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-09 20:11:02 +0330

    DataFrames & Series Working with Rows & Columns

--- day-25/main.py
+++ day-25/main.py
@@ -1,19 +1,34 @@
-# with open("weather-data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-# -----------
-# import csv
-#
-# with open("weather-data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-# -----------
 import pandas
 
-data = pandas.read_csv("weather-data.csv")
+# data = pandas.read_csv("weather-data.csv")
+# data_dict = data.to_dict()
+# temp_list = data["temp"].to_list()
 
-print(data["temp"])
+# # average
+# print((sum(temp_list))/len(temp_list))
+#
+# print(data["temp"].mean())
+
+# # max value
+# print(data["temp"].max())
+
+# # get data in columns
+# print(data["temp"])
+# print(data.temp)
+
+# # get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9 / 5 + 32
+# print(monday_temp_F)
+
+# # create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
