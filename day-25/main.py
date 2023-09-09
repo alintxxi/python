81131b309ae23fada1566aa37a50e0ea42b1fbd7
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-09 23:16:53 +0330

    The Great Squirrel Census Data Analysis (with Pandas!)

--- day-25/main.py
+++ day-25/main.py
@@ -26,9 +26,32 @@ import pandas
 # print(monday_temp_F)
 
 # # create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+# # __________
+# import pandas
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
+# ex_data = data["Primary Fur Color"].value_counts()
+#
+# data_frame = pandas.DataFrame(ex_data)
+# data_frame.to_csv("squirrel_count.csv")
+# # __________
+
+import pandas
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel_count, cinnamon_squirrel_count, black_squirrel_count]
 }
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
