e5a56bb8f3de6fe5139f52840a713127b499944c
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-11 22:35:06 +0330

    Reading CSV Data in Python

---
+++ pyProject/main.py
@@ -0,0 +1,26 @@
+# create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# with open("weather-data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+# with open("weather-data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+# print(temperature)
+
+import pandas
+
+data = pandas.read_csv("weather-data.csv")
+data["temp"]
