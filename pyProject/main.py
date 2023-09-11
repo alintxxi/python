e4e6b10b255aef6400dc67804d3deaeb9f0fbb72
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-11 23:40:01 +0330

    Add files via upload

--- pyProject/main.py
+++ pyProject/main.py
@@ -20,13 +20,29 @@
 #             temperature.append(int(row[1]))
 # print(temperature)
 
+# import pandas
+#
+# data = pandas.read_csv("weather-data.csv")
+# # temp_list = data["temp"].to_list()
+# # avg = sum(temp_list) / len(temp_list)
+#
+# monday = data[data.day == "Monday"]
+# # (0°C × 9/5) + 32 = 32°F
+# monday_temp_f = (monday.temp * 9 / 5) + 32
+# print(monday_temp_f)
+
 import pandas
 
-data = pandas.read_csv("weather-data.csv")
-# temp_list = data["temp"].to_list()
-# avg = sum(temp_list) / len(temp_list)
+data = pandas.read_csv("2018-Central-Park-Squirrel-Census-Squirrel-Data.csv")
 
-monday = data[data.day == "Monday"]
-# (0°C × 9/5) + 32 = 32°F
-monday_temp_f = (monday.temp * 9 / 5) + 32
-print(monday_temp_f)
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+color_dict = {
+    "fur color": ["Gray", "Cinnamon", "Black"],
+    "count": [gray_squirrel_count, cinnamon_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(color_dict)
+print(df)
