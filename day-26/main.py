8eb8c9845c3aec68ca7e671169faf29db927e068
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-12 13:05:53 +0330

    Exercise] Data Overlap

--- day-26/main.py
+++ day-26/main.py
@@ -21,18 +21,31 @@
 
 # # ___[Interactive Coding Exercise] Squaring Numbers___________________________________
 
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# Do Not Change the code above
-# Write your 1 line code below:
-squared_numbers = [num ** 2 for num in numbers]
-# Write your code above:
-print(squared_numbers)
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # Do Not Change the code above
+# # Write your 1 line code below:
+# squared_numbers = [num ** 2 for num in numbers]
+# # Write your code above:
+# print(squared_numbers)
 
 # # ___[Interactive Coding Exercise] Filtering Even Numbers_____________________________
 
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# Do Not Change the code above
-# Write your 1 line code below:
-result = [num for num in numbers if num % 2 == 0]
-# Write your code above:
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # Do Not Change the code above
+# # Write your 1 line code below:
+# result = [num for num in numbers if num % 2 == 0]
+# # Write your code above:
+# print(result)
+
+# # ___[Interactive Coding Exercise] Data Overlap_______________________________________
+
+with open("file1.txt") as file1:
+    data1 = file1.readlines()
+with open("file2.txt") as file2:
+    data2 = file2.readlines()
+result = [int(num) for num in data1 if num in data2 ]
 print(result)
+
+result = [int(num) for num in open("file1.txt") if num in open("file2.txt")]
+print(result)
+
