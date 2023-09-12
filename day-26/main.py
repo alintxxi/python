9a36a0f03ab6f3f526a7883454f9602d88533926
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-12 10:45:29 +0330

    Exercise] Squaring Numbers

--- day-26/main.py
+++ day-26/main.py
@@ -1,20 +1,29 @@
-numbers = [1, 2, 3]
-new_number = [n + 1 for n in numbers]
-print(new_number)
+# numbers = [1, 2, 3]
+# new_number = [n + 1 for n in numbers]
+# print(new_number)
+#
+#
+# name = "Angela"
+# letter_list = [letter for letter in name]
+# print(letter_list)
+#
+#
+# range_list = [num * 2 for num in range(1, 5)]
+# print(range_list)
+#
+#
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+#
+# long_names = [name.upper() for name in names if len(name) > 5]
+# print(long_names)
 
+# # ___[Interactive Coding Exercise] Squaring Numbers___________________________________
 
-name = "Angela"
-letter_list = [letter for letter in name]
-print(letter_list)
-
-
-range_list = [num * 2 for num in range(1, 5)]
-print(range_list)
-
-
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-short_names = [name for name in names if len(name) < 5]
-print(short_names)
-
-long_names = [name.upper() for name in names if len(name) > 5]
-print(long_names)
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Do Not Change the code above
+# Write your 1 line code below:
+squared_numbers = [num ** 2 for num in numbers]
+# Write your code above:
+print(squared_numbers)
