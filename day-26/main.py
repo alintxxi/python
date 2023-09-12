3c123a0f8c2b311636d3f3628713895c579973a3
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-12 12:17:54 +0330

    Exercise] Filtering Even Numbers

--- day-26/main.py
+++ day-26/main.py
@@ -27,3 +27,12 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [num ** 2 for num in numbers]
 # Write your code above:
 print(squared_numbers)
+
+# # ___[Interactive Coding Exercise] Filtering Even Numbers_____________________________
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Do Not Change the code above
+# Write your 1 line code below:
+result = [num for num in numbers if num % 2 == 0]
+# Write your code above:
+print(result)
