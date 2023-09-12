c52c5c0ed17d4d84cbe91ac1feb7eb2515b69118
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-12 23:02:37 +0330

    Iterate over a Pandas DataFrame

--- day-26/main.py
+++ day-26/main.py
@@ -71,18 +71,41 @@
 
 # # ___[Interactive Coding Exercise] Dictionary Comprehension 2_________________________
 
-weather_c = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+# # Don't Change code above
+#
+# # Write your code below:
+# weather_f = {day: (temp_c * 9 / 5) + 32 for (day, temp_c) in weather_c.items()}
+#
+# print(weather_f)
+
+# # ___How to Iterate over a Pandas DataFrame_________________________________________
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
 }
-# Don't Change code above
 
-# Write your code below:
-weather_f = {day: (temp_c * 9 / 5) + 32 for (day, temp_c) in weather_c.items()}
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    print(value)
 
-print(weather_f)
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+#Loop through a data frame:
+for (key, value) in student_data_frame.items()
+    print(value)
+
+#Loop through rows of a data frame:
+for (index, row) in student_data_frame.iterrows():
+    print(row.student)
