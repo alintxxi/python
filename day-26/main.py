57a4246c638b2dddedb49a750cdf63132898039e
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-12 18:11:26 +0330

    Exercise] Dictionary Comprehension 2

--- day-26/main.py
+++ day-26/main.py
@@ -61,10 +61,28 @@
 
 # # ___[Interactive Coding Exercise] Dictionary Comprehension 1_________________________
 
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# # Don't Change code above
+#
+# # Write your code below:
+# result = {word: len(word) for (word) in sentence.split()}
+#
+# print(result)
+
+# # ___[Interactive Coding Exercise] Dictionary Comprehension 2_________________________
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
 # Don't Change code above
 
 # Write your code below:
-result = {word: len(word) for (word) in sentence.split()}
+weather_f = {day: (temp_c * 9 / 5) + 32 for (day, temp_c) in weather_c.items()}
 
-print(result)
+print(weather_f)
