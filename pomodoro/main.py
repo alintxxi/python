f7ea6b63e88fc2153328db798608d1fab03cb2d2
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-15 22:37:17 +0330

    Dynamic Typing Explained

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -16,15 +16,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(300)
+    count_down(12)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    # if count_sec < 10:
+    #     count_sec = f"0{count_sec}"
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    canvas.itemconfig(timer_text, text=f"{count_min:02}:{count_sec:02}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
