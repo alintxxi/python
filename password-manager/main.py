2bfea6c05f2cd2dde011b98c57d75715c949991d
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-16 20:03:29 +0330

    Dialog Boxes and Pop-Ups in Tkinter

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -8,10 +9,17 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {username} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nUsername: {username}"
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {username} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
