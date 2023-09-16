572a2ff5ab837e931e0488fc890fe7671662f4eb
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-16 17:30:48 +0330

    Sticky grid but doesn't work on button

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -7,45 +6,34 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-website_label = Label(text="Website: ")
+# Labels
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
-
-username_label = Label(text="Email/Username: ")
+username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
-
-username_entry = Entry(width=35)
-username_entry.grid(column=1, row=2, columnspan=2)
-
-password_label = Label(text="Password: ")
+password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+# Entries
+website_entry = Entry(width=43)
+website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
+username_entry = Entry(width=43)
+username_entry.grid(column=1, row=2, columnspan=2, sticky=W)
+password_entry = Entry(width=23)
+password_entry.grid(column=1, row=3, sticky=W)
 
-password_button = Button(text="Generate Password")
-password_button.grid(column=2, row=3)
-
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3, sticky=W)
 add_button = Button(text="Add", width=36)
-add_button.grid(column=1, row=4, columnspan=2)
-
-
-
-
-
-
-
-
-
+add_button.grid(column=1, row=4, columnspan=2, sticky=W)
 
 
 
