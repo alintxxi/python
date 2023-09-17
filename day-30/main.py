d07e9ebc1843560da097abbc90a4f8342c150f50
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-18 00:02:13 +0330

    Some note

--- day-30/main.py
+++ day-30/main.py
@@ -3,15 +3,15 @@
 try:
     file = open("a_file.txt")
     a_dictionary = {"key": "value"}
-    print(a_dictionary["sdsafs"]) # after 2 times run the code then change "sdsafs" to "key" and run again
+    print(a_dictionary["sdsafs"])  # after 2 times run the code then change "sdsafs" to "key" and run again
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("something")
 except KeyError as error_message:
     print(f"The key {error_message} does not exist.")
-else:
+else:  # When all of "try" section goes successfully then jump to this section.
     content = file.read()
     print(content)
-finally:
+finally:  # Run no matter what happened.
     file.close()
     print("File was closed.")
