c24d7baee6e4b40f96c6275f8e2dbd9b2bbabd82
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-18 22:34:09 +0330

    Exercise] KeyError Handling

--- day-30/main.py
+++ day-30/main.py
@@ -32,17 +32,38 @@
 
 # # 004 [Interactive Coding Exercise] IndexError Handling
 
-fruits = ["Apple", "Pear", "Orange"]
+# fruits = ["Apple", "Pear", "Orange"]
+#
+#
+# # TODO: Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("Fruit pie")
+#     else:
+#         print(fruit + " pie")
+#
+#
+# make_pie(4)
 
+# # 005 [Interactive Coding Exercise] KeyError Handling
 
-# TODO: Catch the exception and make sure the code runs without crashing.
-def make_pie(index):
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
     try:
-        fruit = fruits[index]
-    except IndexError:
-        print("Fruit pie")
-    else:
-        print(fruit + " pie")
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass  # OR total_likes += 0
 
-
-make_pie(4)
+print(total_likes)
