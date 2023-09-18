9ac84cd44887af93829b570bd0108b2682ce0c27
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-18 23:16:22 +0330

    try, except and else

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -24,15 +24,24 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
 # print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-word = input("Enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in word]
-print(output_list)
 
+generate_phonetic()
