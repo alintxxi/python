60639849a6d810b143b5789b2db8f3260ca12c5d
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-09-21 23:36:31 +0330

    Find the problem!!!

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,13 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     password_entry.delete(0, END)
-
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v',
                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
@@ -16,19 +14,15 @@ def generate_password():
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-
     password_list = password_letters + password_numbers + password_symbols
     shuffle(password_list)
-
     password = "".join(password_list)
     # password = ""
     # for char in password_list:
     #     password += char
-
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
@@ -38,12 +32,6 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    new_data = {
-        website: {
-            "username": username,
-            "password": password,
-        }
-    }
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
@@ -51,21 +39,8 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nUsername: {username}"
                                                               f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            try:
-                with open("data.json", "r") as data_file:
-                    # Reading old data
-                    data = json.load(data_file)
-            except FileNotFoundError:
-                with open("data.json", "w") as data_file:
-                    json.dump(new_data, data_file, indent=4)
-            else:
-                # Updating old data with new data
-                data.update(new_data)
-
-                with open("data.json", "w") as data_file:
-                    # Saving updated data
-                    json.dump(new_data, data_file, indent=4)
-            finally:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {username} | {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
@@ -74,12 +49,10 @@ def save():
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
-
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
-
 # Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
@@ -87,7 +60,6 @@ username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
-
 # Entries
 website_entry = Entry(width=43)
 website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
@@ -97,11 +69,9 @@ username_entry.grid(column=1, row=2, columnspan=2, sticky=W)
 username_entry.insert(0, "angela@gmail.com")  # Fill with something as default
 password_entry = Entry(width=23)
 password_entry.grid(column=1, row=3, sticky=W)
-
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3, sticky=W)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky=W)
-
 window.mainloop()
