684715b7f8f25a59f65d12872b65465087aaa4c3
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-10-04 01:02:23 +0330

    Run Your Python Code in the Cloud!

--- day-32-birthday-wisher/main.py
+++ day-32-birthday-wisher/main.py
@@ -57,15 +57,11 @@
 
 # # 006 Automated Birthday Wisher Project Challenge
 ##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true,
-# pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+# # 1. Update the birthdays.csv
+# # 2. Check if today matches a birthday in the birthdays.csv
+# # 3. If step 2 is true,
+# # pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# # 4. Send the letter generated in step 3 to that person's email address.
 
 import smtplib
 from datetime import datetime
@@ -95,3 +91,7 @@ if today_tuple in birthdays_dict:
             to_addrs=birthday_person["email"],
             msg=f"subject:Happy Birthday {birthday_person['name']}\n\n{contents}"
         )
+
+# # 007 Solution & Walkthrough for the Automated Birthday Wisher
+# # 008 Run Your Python Code in the Cloud!
+# # pythonanywhere.org
