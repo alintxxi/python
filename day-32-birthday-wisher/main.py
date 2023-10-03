3a1ad1988999f08d8ddd3020506b751e41e1e00c
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-10-03 23:23:09 +0330

    Automated Birthday Wisher Project Challenge

--- day-32-birthday-wisher/main.py
+++ day-32-birthday-wisher/main.py
@@ -15,6 +15,7 @@
 #         msg="Subject:Hello\n\nThis is the body of my email."
 #     )
 
+
 # # 004 Working with the datetime Module
 # import datetime as dt
 #
@@ -27,28 +28,70 @@
 # date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
 # print(date_of_birth)
 
+
 # # 005 Challenge 1 - Send Motivational Quotes on Mondays via Email
+# import smtplib
+# import datetime as dt
+# import random
+#
+# MY_EMAIL = "alintxxipython@gmail.com"
+# MY_PASSWORD = "jnwxzrbemsckbxxu"
+#
+# now = dt.datetime.now()
+# weekday = now.weekday()
+# if weekday == 1:
+#     with open("quotes.txt") as quote_file:
+#         all_quotes = quote_file.readlines()
+#         quote = random.choice(all_quotes)
+#
+#     print(quote)
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(MY_EMAIL, MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs=MY_EMAIL,
+#             msg=f"subject:Monday Motivation\n\n{quote}"
+#         )
+
+
+# # 006 Automated Birthday Wisher Project Challenge
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true,
+# pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
 import smtplib
-import datetime as dt
+from datetime import datetime
+import pandas
 import random
 
 MY_EMAIL = "alintxxipython@gmail.com"
 MY_PASSWORD = "jnwxzrbemsckbxxu"
 
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-    print(quote)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=MY_EMAIL,
-            msg=f"subject:Monday Motivation\n\n{quote}"
+            to_addrs=birthday_person["email"],
+            msg=f"subject:Happy Birthday {birthday_person['name']}\n\n{contents}"
         )
-    
