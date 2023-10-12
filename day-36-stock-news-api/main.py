17b1440e00c243bc8418b84ad78297b1cb410f9b
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-10-13 00:19:32 +0330

    Send the SMS Messages

--- day-36-stock-news-api/main.py
+++ day-36-stock-news-api/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "NG4P4ZCRA81D9QH2"
 NEWS_API_KEY = "1237fad93d7c4705a9e8c14e233fb92c"
+TWILIO_SID = "AC123c80e05283e5e81a19328190cd0839"
+TWILIO_AUTH_TOKEN = "5eabfcf6f564b8c4331985dc3f634b47"
 
 # # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -29,12 +32,19 @@ yesterday_closing_price = float(yesterday_data["4. close"])
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = float(day_before_yesterday_data["4. close"])
 
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
 
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
 # # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-if diff_percent >= 5:
+if abs(diff_percent) < 0:
     news_params = {
         "qInTitle": COMPANY_NAME,
         "apiKey": NEWS_API_KEY,
@@ -46,17 +56,16 @@ if diff_percent >= 5:
 
     three_articles = articles[:3]
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# # STEP 3: Use https://www.twilio.com
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
+    formatted_article = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}.\nBrief: {article['description']}" for article in three_articles]
 
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_article:
+        message = client.messages.create(
+            body=article,
+            from_="+12673092068",
+            to="+989127374348",
+        )
