3b7295a856976301ca27a3656ec2c9a8d0705e60
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-10-15 08:54:49 +0330

    Workout Tracking Using Google Sheets

---
+++ day-38-exercise-tracker/main.py
@@ -0,0 +1,8 @@
+# # Day 38
+# # Intermediate+
+# # Workout Tracking Using Google Sheets
+
+# # 001 Day 38 Goals what you will make by the end of the day
+# # 002 Step 1 - Setup API Credentials and Google Spreadsheet
+APP_ID = "960d287e"
+API_KEY = "aeab12446674a813729513acd7b32842—"
