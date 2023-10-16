9fc1422105c6ac1124f43c9db160b48edeaeffdc
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-10-16 23:20:12 +0330

    Saving Data into Google Sheets

--- day-38-exercise-tracker/main.py
+++ day-38-exercise-tracker/main.py
@@ -4,6 +4,7 @@
 
 # # 001 Day 38 Goals what you will make by the end of the day
 import requests
+from datetime import datetime
 
 # # 002 Step 1 - Setup API Credentials and Google Spreadsheet
 GENDER = "Male"
@@ -34,6 +35,26 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
 
 # # 004 Step 3 - Setup Your Google Sheet with Sheety
+# # 005 Step 4 - Saving Data into Google Sheets
+sheet_endpoint = "https://api.sheety.co/c5b12b0d32f64a9250892842f01ce886/myWorkouts/workouts"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_input = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_input)
+
+    print(sheet_response.text)
+
