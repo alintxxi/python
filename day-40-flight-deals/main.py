ebbb6c168a1bf61b1e3fb07b21129c5839896489
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-11-02 21:11:52 +0330

    Email all our customers

--- day-40-flight-deals/main.py
+++ day-40-flight-deals/main.py
@@ -34,16 +34,18 @@ for destination_code in destinations:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+    print(flight.price)
     if flight is None:
         continue
 
     if flight.price < destinations[destination_code]["price"]:
+
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
+
         message = f"Low price alert! Only £{flight.price}GBP to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-
-        ################
         if flight.stop_overs > 0:
-            message += f"\n\nFlight has {flight.stop_overs}, via {flight.via_city}."
-            print(message)
-        ################
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
 
-        notification_manager.send_sms(message)
+        notification_manager.send_emails(emails, message)
--- day-40-flight-deals/notification_manager.py
+++ day-40-flight-deals/notification_manager.py
@@ -1,3 +1,4 @@
+import smtplib
 from twilio.rest import Client
 
 TWILIO_SID = YOUR TWILIO ACCOUNT SID
@@ -19,3 +20,14 @@ class NotificationManager:
         )
         # Prints if successfully sent.
         print(message.sid)
+
+    def send_emails(self, emails, message):
+        with smtplib.SMTP(EMAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode(utf-8)
+                )
