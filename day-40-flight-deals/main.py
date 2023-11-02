44c5a74e02424a5743532a4c6702349c7cdf5f03
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-11-02 18:21:50 +0330

    Download the Starting Project

--- day-40-flight-deals/main.py
+++ day-40-flight-deals/main.py
@@ -1,23 +1,32 @@
-import sheety
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
-print("Welcome to John's Flight Club.\n \
-We find the best flight deals and email them to you.")
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
-first_name = input("What is your first name? ").title()
-last_name = input("What is your last name? ").title()
+ORIGIN_CITY_IATA = "LON"
 
-email1 = "email1"
-email2 = "email2"
-while email1 != email2:
-    email1 = input("What is your email? ")
-    if email1.lower() == "quit" \
-            or email1.lower() == "exit":
-        exit()
-    email2 = input("Please verify your email : ")
-    if email2.lower() == "quit" \
-            or email2.lower() == "exit":
-        exit()
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
 
-print("OK. You're in the club!")
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-sheety.post_new_row(first_name, last_name, email1)
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
