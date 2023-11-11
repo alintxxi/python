3f8261c2adde932d21eb2279b270b01b4aceedf6
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-11-11 23:52:41 +0330

    Finding and Selecting Particular Elements with BeautifulSoup

--- day-45-bs4/main.py
+++ day-45-bs4/main.py
@@ -1,3 +1,6 @@
+# # 001 Day 45 Goals what you will make by the end of the day
+# # 002 Parsing HTML and Making Soup
+
 from bs4 import BeautifulSoup
 # import lxml
 
@@ -11,3 +14,29 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.prettify())
 
 # print(soup.p)
+
+# # 003 Finding and Selecting Particular Elements with BeautifulSoup
+
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+# all_anchor_tags = soup.find_all(name="a")
+# for tag in all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+# name = soup.select_one(selector="#name")
+# print(name)
+
+# headings = soup.select(".heading")
+# print(headings)
+
