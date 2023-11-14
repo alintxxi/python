637ab32bbf11a27d96f1dba6d6c43def2b1246e0
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-11-14 09:19:12 +0330

    Scraping a Live Website

--- day-45-bs4/main.py
+++ day-45-bs4/main.py
@@ -1,13 +1,13 @@
 # # 001 Day 45 Goals what you will make by the end of the day
 # # 002 Parsing HTML and Making Soup
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 # import lxml
 
-with open("website.html") as file:
-    contents = file.read()
+# with open("website.html") as file:
+#     contents = file.read()
 
-soup = BeautifulSoup(contents, "html.parser")
+# soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title)
 # print(soup.title.string)
 
@@ -40,3 +40,27 @@ soup = BeautifulSoup(contents, "html.parser")
 # headings = soup.select(".heading")
 # print(headings)
 
+# # 004 Scraping a Live Website
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="a", rel="noreferrer")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
