53a6ad85a1e4076dbb061390b872163e46bb8112
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-11-17 22:33:23 +0330

    Search Spotify for the Songs from Step 1

--- day-46-spotify-playlist/main.py
+++ day-46-spotify-playlist/main.py
@@ -13,7 +13,6 @@ song_names_spans = soup.select("li ul li h3")
 
 song_names = [song.getText().strip() for song in song_names_spans]
 
-print(song_names)
 
 # # 003 Step 2 - Authentication with Spotify
 Client_ID = "f7590b25b02b462ebe0fdcb9963a9c7e"
@@ -34,3 +33,16 @@ sp = spotipy.Spotify(
 )
 # https://example.com/?code=AQDeF3QLCrjEByPykhi24FxykDSf6eUFRy2Wpz2-qzs5ca_o5JTGJljLQMTG75YQzmSHOSDV2raMdhFLuCTJq-pMMad1Z6g0phnE-TmLLizvSYGmyZZL0r1FC3ry0PSmYydkQ37_CBlYoNIuZGeZMr0hP50eaRqGtjLBSmLwX9jEeBh6Bi65Myn95gzBe9IN
 user_id = sp.current_user()["id"]
+
+
+# # 004 Step 3 - Search Spotify for the Songs from Step 1
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
