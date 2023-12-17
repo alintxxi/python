225e12e5c4013d1615a566759021d5506ee579a5
alintxxi <50053163+alintxxi@users.noreply.github.com>
2023-12-17 17:59:15 +0330

    Use Selenium to Scrape Website Data

--- day-48-selenium/main.py
+++ day-48-selenium/main.py
@@ -14,15 +14,28 @@ driver.get("https://www.python.org/")
 # search_bar = driver.find_element("name", "q")
 # print(search_bar.tag_name)
 # print(search_bar.get_attribute("placeholder"))
-#
+
 # logo = driver.find_element(By.CLASS_NAME, "python-logo")
 # print(logo.size)
 
 # documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
 # print(documentation_link.text)
 
-bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
+# bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+# # 004 Challenge Use Selenium to Scrape Website Data
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+
+print(events)
 
 # driver.close()
 driver.quit()
